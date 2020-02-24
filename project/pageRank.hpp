bc383871977912ccfd3edf8ea7561438f881e0c9
Yiming Sun <sym199512@gmail.com>
2020-02-23 20:00:54 -0800

    soa

--- project/pageRank.hpp
+++ project/pageRank.hpp
@@ -7,21 +7,31 @@ using namespace std;
 
 namespace csc586 {
 
-using Id = int;
-using Count = int;
+using Id = unsigned short int;
+using Count = unsigned short int;
 using Score = float;
 
 namespace aos {
 
 struct Node
 {
-	Id id;
-	vector< Node* > nodesFrom;
-	//vector< Node* > nodesTo;
-	//Count countFrom;
-	Count countTo;
-	Score scorePrev; // Score in iteration i-1;
-	Score score; // Score in iteration i;
+	vector< Node* > nodesFrom; // 8*n bytes.
+	Score scorePrev; // Score in iteration i-1; 4 bytes.
+	Score score; // Score in iteration i; 4 bytes.
+	Id id; // 2 bytes.
+	Count countTo; // 2 bytes.
+};
+
+}
+
+namespace soa {
+
+struct Nodes
+{
+	vector< vector< Id > > nodesFrom;
+	vector< Count > countTo;
+	vector< Score > score;
+	vector< Score > scorePrev;
 };
 
 }
