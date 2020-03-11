32c57bd9deb7c586672aa3474a70628586d63bac
Yiming Sun <sym199512@gmail.com>
2020-03-11 14:16:18 -0700

    added pragma to for loops

--- pagerank_threaded/soa.cpp
+++ pagerank_threaded/soa.cpp
@@ -11,57 +11,55 @@ using namespace std;
 using namespace csc586;
 using namespace csc586::soa;
 
-static const string filename = "../test/erdos-100000.txt";
+static const string filename = "../test/erdos-10000.txt";
 static const int nIter = 10; // Number of iterations.
 static const float d = 0.85; // Damping factor.
+static const int NUM_THREADS = 4;
 
 // PageRank Algorithm.
 void pageRank(Nodes *nodes)
 {
 	int n = nodes->nodesFrom.size();
 
-	//int count = 0;
+	// Deal with dangling nodes problem.
 	//#pragma omp parallel for
 	for (auto i = 0; i < nodes->countTo.size(); ++i)
 	{
 		// Dangling node: no outgoing nodes.
 		if (nodes->countTo[i] == 0)
 		{
-			// Let it outgoing to everyone!
+			// Let it go to everyone (includes itself)!
 			nodes->countTo[i] = n;
 
 			for (auto j = 0; j < n; ++j)
 			{
 				nodes->nodesFrom[j].push_back(i);
 			}
-
-			//count++;
 		}
 	}
 
-	//cout << count << endl;
-	// Iteration.
+	// Iterations.
 	for (auto iter = 0; iter < nIter; ++iter)
 	{
 
 		if (iter == 0)
 		{
 			// Initialize scores.
-			//#pragma omp parallel for
+			#pragma omp parallel for num_threads(NUM_THREADS)
 			for (auto i = 0; i < n; ++i)
 			{
 				nodes->score[i] = 1.0 / n;
 			}
 		} else {
 			// Move current score to previous score, and assign (1-d)/n to each current score (do addition later).
-			//#pragma omp parallel for
+			#pragma omp parallel for num_threads(NUM_THREADS)
 			for (auto i = 0; i < n; ++i)
 			{
 				nodes->scorePrev[i] = nodes->score[i];
 				nodes->score[i] = (1 - d)/n;
 			}
 			// For every node i, visit the nodes *j going to node i (node *j -> node i).
-			#pragma omp parallel for num_threads(4)
+			#pragma omp parallel for num_threads(NUM_THREADS)
 			for (auto i = 0; i < n; ++i)
 			{
 				for (vector< Id >::iterator j = nodes->nodesFrom[i].begin(); j != nodes->nodesFrom[i].end(); ++j)
@@ -69,10 +67,6 @@ void pageRank(Nodes *nodes)
 					// cout << *j << endl;
 					nodes->score[i] += (nodes->scorePrev[*j]/nodes->countTo[*j])*d;
 				}
-				// for (auto j = 0; j < nodes->nodesFrom[i].size(); ++j)
-				// {
-				// 	nodes->score[i] += (nodes->scorePrev[nodes->nodesFrom[i][j]]/nodes->countTo[nodes->nodesFrom[i][j]])*d;
-				// }
 			}
 		}
 	}
@@ -142,7 +136,7 @@ int main()
 	float sum = 0.0;
 	for (long unsigned int i = 0; i < nodes->nodesFrom.size(); ++i)
 	{
-		//cout << i << " = " << nodes->score[i] << endl;
+		// cout << i << " = " << nodes->score[i] << endl;
 		sum += nodes->score[i];
 	}
 	cout << "sum = " << sum << endl;
