4044f62349b728a837371d1fe98318314cdb7ec0
SiRumCZ <sirumcz@gmail.com>
2020-04-22 18:03:39 -0700

    update repo

--- project_final_version/plot.py
+++ project_final_version/plot.py
@@ -8,8 +8,8 @@ n = None
 mode = None
 filenames = []
 num_nodes = []
-pr_time = {"bl": [], "cpu": [], "gpu": []}
-total_time = {"bl": [], "cpu": [], "gpu": []}
+pr_time = {"naive":[], "bl": [], "cpu": [], "gpu": []}
+total_time = {"naive":[], "bl": [], "cpu": [], "gpu": []}
 
 with open("results.txt", "r") as f:
 	first = True
@@ -33,7 +33,7 @@ print(num_nodes)
 os.system("mkdir plots")
 
 # plot runtimes (update, pagerank, total) for each mode
-for mode in ["bl", "cpu", "gpu"]:
+for mode in ["naive", "bl", "cpu", "gpu"]:
 	plt.figure()
 	plt.scatter(num_nodes, pr_time[mode])
 	plt.plot(num_nodes, pr_time[mode], label="PR Calculation Time")
@@ -52,8 +52,9 @@ gpu_spdup = [pr_time["bl"][i]/pr_time["gpu"][i] for i in range(n)]
 plt.figure()
 
 fig, ax1 = plt.subplots()
-ax1.bar(X + 0.25, pr_time["bl"], color='#95E1EF', width=0.25, label = "Baseline")
-ax1.bar(X, total_time["cpu"], color='#FEDBC6', width=0.25, label = "CPU Parallelism")
+# ax1.bar(X + 0.5, pr_time["naive"], color='#000000', width=0.25, label = "Naive")
+ax1.bar(X + 0.25, pr_time["bl"], color='#95E1EF', width=0.25, label = "Single Core Baseline")
+ax1.bar(X, pr_time["cpu"], color='#FEDBC6', width=0.25, label = "CPU Parallelism")
 ax1.bar(X - 0.25, pr_time["gpu"], color='#881600', width=0.25, label="GPGPU Parallelism")
 ax1.set_ylabel("Total Runtime (microseconds)")
 ax1.legend()
